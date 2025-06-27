a78c72064c037497c9c739f509d68d0f5b7b39da
Benedict <bergben44@gmail.com>
2025-06-27 11:12:56 +0200

    proof of concept

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use serde::Deserialize;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+pub fn main() {
+    println!("hello world");
+
+    let res = call_tool("test_tool", serde_json::json!({"a": "hello", "b": 42}));
+    println!("result: {:?}", res);
+}
+
+fn test_tool(a: String, b: u32) -> Result<u32> {
+    println!("a: {}, b: {}", a, b);
+    Ok(42)
+}
+
+fn call_tool(tool_name: &str, args: serde_json::Value) -> Result<serde_json::Value> {
+    match tool_name {
+        "test_tool" => Ok(serde_json::to_value(test_tool.call((
+            get_value(&args, "a")?,
+            get_value(&args, "b")?,
+        ))?)?),
+        _ => Err("no valid tool name".into())
+    }
+}
+
+fn get_value<T: for <'a> Deserialize<'a>>(root: &serde_json::Value, name: &str) -> Result<T> {
+    Ok(serde_json::from_value(
+        root
+            .get(name)
+            .ok_or(format!("Missing or invalid '{}' parameter", name))?
+            .clone()
+    )?)
+}
+
+trait Call2<A, B, Z> {
+    fn call(self, args: (A, B)) -> Z;
+}
+
+impl<F, A, B, Z> Call2<A, B, Z> for F
+where
+    F: FnOnce(A, B) -> Z,
+{
+    fn call(self, (a, b): (A, B)) -> Z {
+        self(a, b)
+    }
+}
